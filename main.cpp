658fdff0b78659f2940a8d668dc71b928cd1af38
Aditya-Singh2008 <96719766+Aditya-Singh2008@users.noreply.github.com>
2023-07-16 22:04:25 -0700

    Resolved Push and Pull Issues in  main.cpp
    There were 2 different versions, this is the one currently being used

--- main.cpp
+++ main.cpp
@@ -2,22 +2,27 @@
 #include <iostream>
 
 int main(){
-
+    
     std::cout << "New Perceptron:\n\n";
 
     Perceptron *newPerceptron = new Perceptron(2, 10, 1);
 
     newPerceptron->fillData();
     newPerceptron->printArray();
-    std::cout << "\n";
-    for(int i = 0; i < 10; i++){
-        newPerceptron->feedForward(i);
-    }
-    newPerceptron->printOut();
+    //std::cout << "FF:\n";
+    //for(int i = 0; i < 10; i++){
+        //newPerceptron->feedForward(i);
+    //}
+    //newPerceptron->printOut();
 
     for(int i = 0; i < 10; i++){
-        std::cout << "\n";
+        std::cout << "\nFF:\n";
+        std::cout << newPerceptron->feedForward(i);
+        std::cout << "\nLSS\n";
         std::cout << newPerceptron->lossFunction(i);
+        std::cout << "\nLSSP\n";
+        std::cout << newPerceptron->lossFunctionPrime(i);
+        std::cout << "\n";
     }
 
     
